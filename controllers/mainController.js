c292b868aebea42bd82de39615de9df276898a78
Cristian <cristianramos92639@gmail.com>
2021-12-06 19:59:30 -0300

    edite main.js

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../data/productosDataBase.json');
+const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 function saveProducts() {
