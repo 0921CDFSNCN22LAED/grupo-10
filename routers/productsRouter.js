18786d4e2df6497f7fd3c1314386df5f813f1615
Flechashe <se.amigos@gmail.com>
2021-12-06 21:19:31 -0300

    Trabajando en ruta de productos.

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -2,5 +2,17 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 
-router.get('/')
-router.get('/:id')
+// Listado de productos
+router.get('/');
+// Acción de creación
+router.post('/');
+// Formulario de creación de productos
+router.get('/create');
+// Detalle de un producto
+router.get('/:id');
+// Formulario de edición de productos
+router.get('/:id/edit');
+// Acción de edición
+router.put('/:id');
+// Acción de borrado
+router.delete('/:id');
