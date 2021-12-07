f3aa906050fe1a76f915a5f66308de6d7452c3bf
Flechashe <se.amigos@gmail.com>
2021-12-06 21:11:13 -0300

    Creé ruta de productos.

---
+++ routers/productsRouter.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const productsController = require('../controllers/productsController');
+
+router.get('/')
+router.get('/:id')
