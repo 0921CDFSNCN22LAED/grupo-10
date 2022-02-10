e457b7f1e63d19cf2fb5d7818d1c38ebbd1f935a
LuiosD <luisdartayet@gmail.com>
2022-02-10 09:36:23 -0300

    corrijo una C, y me dejo un recordatorio de no hacer las cosas apurado

--- database/seeders/20220210120936-user.js
+++ database/seeders/20220210120936-user.js
@@ -40,21 +40,21 @@ module.exports = {
         profileImage: '1644019570246.png',
       },
       {
-        id: 1642110610473,
+        id: 5,
         name: 'Jose',
         email: 'jose@mail.com',
         password:
           '$2a$10$Pz99vQ0UhbpRkLzWdYc5A.5tpFQSE9VZEkBTKxUa5KKNCGiZUMPtO',
-        roleLevel: 'Client',
+        roleLevel: 'client',
         profileImage: 'default-avatar.png',
       },
       {
-        id: 1642110635393,
+        id: 6,
         name: 'Oriana',
         email: 'oriana@mail.com',
         password:
           '$2a$10$li12N1zGiGvJ9lxZIJC0Se9cCk6KPSi0j/fLqPmMwd/5Eymn5nEt.',
-        roleLevel: 'Client',
+        roleLevel: 'client',
         profileImage: '1642110635362.png',
       },
     ]);
