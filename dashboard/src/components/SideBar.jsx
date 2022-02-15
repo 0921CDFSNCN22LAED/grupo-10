f7c35663e330f6e56532da45d6c7e16ad321c9c9
Cristian <cristianramos92639@gmail.com>
2022-02-15 20:13:28 -0300

    React first commit

---
+++ dashboard/src/components/SideBar.jsx
@@ -0,0 +1,56 @@
+import logo from "../assets/images/PC_Gamer_logo.png" 
+
+function Sidebar(){
+  return <ul class="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+  {/* <!-- Sidebar - Brand --> */}
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+    <div class="sidebar-brand-icon">
+      <img class="w-100" src={logo} alt="Digital House"/>
+    </div>
+  </a>
+
+  {/* <!-- Divider --> */}
+  <hr class="sidebar-divider my-0"/>
+
+  {/* <!-- Nav Item - Dashboard --> */}
+  <li class="nav-item active">
+    <a class="nav-link" href="/">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard - PC GAMER</span></a>
+  </li>
+
+  {/* <!-- Divider --> */}
+  <hr class="sidebar-divider"/>
+
+  {/* <!-- Heading --> */}
+  <div class="sidebar-heading">Actions</div>
+
+  {/* <!-- Nav Item - Pages --> */}
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="/">
+      <i class="fas fa-fw fa-folder"></i>
+      <span>Pages</span>
+    </a>
+  </li>
+
+  {/* <!-- Nav Item - Charts --> */}
+  <li class="nav-item">
+    <a class="nav-link" href="/">
+      <i class="fas fa-fw fa-chart-area"></i>
+      <span>Charts</span></a>
+  </li>
+
+  {/* <!-- Nav Item - Tables --> */}
+  <li class="nav-item">
+    <a class="nav-link" href="/">
+      <i class="fas fa-fw fa-table"></i>
+      <span>Tables</span></a>
+  </li>
+
+  {/* <!-- Divider --> */}
+  <hr class="sidebar-divider d-none d-md-block"/>
+</ul>
+}
+
+export default Sidebar
