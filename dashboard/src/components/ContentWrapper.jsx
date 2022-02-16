c93683e2e09615f26a9b2005fb8f99abdd8b844c
Flechashe <se.amigos@gmail.com>
2022-02-16 01:32:28 -0300

    Cambié class por className para solucionar errores.

--- dashboard/src/components/ContentWrapper.jsx
+++ dashboard/src/components/ContentWrapper.jsx
@@ -2,47 +2,47 @@ import React  from "react"
 import ContentRowTop from "./ContentRowTop"
 
 const ContentWrapper = () =>{
-  return <div id="content-wrapper" class="d-flex flex-column">
+  return <div id="content-wrapper" className="d-flex flex-column">
 
   {/* <!-- Main Content --> */}
   <div id="content">
 
     {/* <!-- Topbar --> */}
-    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
       {/* <!-- Sidebar Toggle (Topbar) --> */}
-      <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-        <i class="fa fa-bars"></i>
+      <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+        <i className="fa fa-bars"></i>
       </button>
 
       {/* <!-- Topbar Navbar --> */}
-      <ul class="navbar-nav ml-auto">
+      <ul className="navbar-nav ml-auto">
 
         {/* <!-- Nav Item - Alerts --> */}
-        <li class="nav-item dropdown no-arrow mx-1">
-          <a class="nav-link dropdown-toggle" href="/" id="alertsDropdown">
-            <i class="fas fa-bell fa-fw"></i>
+        <li className="nav-item dropdown no-arrow mx-1">
+          <a className="nav-link dropdown-toggle" href="/" id="alertsDropdown">
+            <i className="fas fa-bell fa-fw"></i>
             {/* <!-- Counter - Alerts --> */}
-            <span class="badge badge-danger badge-counter">3+</span>
+            <span className="badge badge-danger badge-counter">3+</span>
           </a>
         </li>
 
         {/* <!-- Nav Item - Messages --> */}
-        <li class="nav-item dropdown no-arrow mx-1">
-          <a class="nav-link dropdown-toggle" href="/" id="messagesDropdown">
-            <i class="fas fa-envelope fa-fw"></i>
+        <li className="nav-item dropdown no-arrow mx-1">
+          <a className="nav-link dropdown-toggle" href="/" id="messagesDropdown">
+            <i className="fas fa-envelope fa-fw"></i>
             {/* <!-- Counter - Messages --> */}
-            <span class="badge badge-danger badge-counter">7</span>
+            <span className="badge badge-danger badge-counter">7</span>
           </a>
         </li>
 
-        <div class="topbar-divider d-none d-sm-block"></div>
+        <div className="topbar-divider d-none d-sm-block"></div>
 
         {/* <!-- Nav Item - User Information --> */}
-        <li class="nav-item dropdown no-arrow">
-          <a class="nav-link dropdown-toggle" href="/" id="userDropdown">
-            <span class="mr-2 d-none d-lg-inline text-gray-600 small">Jordan Walke</span>
-            <img class="img-profile rounded-circle" src="assets/images/jordan-walke.png" alt="Jordan Walke - Creador de React" width="60"/>
+        <li className="nav-item dropdown no-arrow">
+          <a className="nav-link dropdown-toggle" href="/" id="userDropdown">
+            <span className="mr-2 d-none d-lg-inline text-gray-600 small">Jordan Walke</span>
+            <img className="img-profile rounded-circle" src="assets/images/jordan-walke.png" alt="Jordan Walke - Creador de React" width="60"/>
           </a>
         </li>
 
@@ -56,9 +56,9 @@ const ContentWrapper = () =>{
   {/* <!-- End of MainContent --> */}
 
   {/* <!-- Footer --> */}
-  <footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-      <div class="copyright text-center my-auto">
+  <footer className="sticky-footer bg-white">
+    <div className="container my-auto">
+      <div className="copyright text-center my-auto">
         <span>Copyright &copy; Dashboard 2021</span>
       </div>
     </div>
--- dashboard/src/components/SideBar.jsx
+++ dashboard/src/components/SideBar.jsx
@@ -1,55 +1,55 @@
 import logo from "../assets/images/PC_Gamer_logo.png" 
 
 function Sidebar(){
-  return <ul class="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+  return <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
 
   {/* <!-- Sidebar - Brand --> */}
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
-    <div class="sidebar-brand-icon">
-      <img class="w-100" src={logo} alt="Digital House"/>
+  <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+    <div className="sidebar-brand-icon">
+      <img className="w-100" src={logo} alt="Digital House"/>
     </div>
   </a>
 
   {/* <!-- Divider --> */}
-  <hr class="sidebar-divider my-0"/>
+  <hr className="sidebar-divider my-0"/>
 
   {/* <!-- Nav Item - Dashboard --> */}
-  <li class="nav-item active">
-    <a class="nav-link" href="/">
-      <i class="fas fa-fw fa-tachometer-alt"></i>
+  <li className="nav-item active">
+    <a className="nav-link" href="/">
+      <i className="fas fa-fw fa-tachometer-alt"></i>
       <span>Dashboard - PC GAMER</span></a>
   </li>
 
   {/* <!-- Divider --> */}
-  <hr class="sidebar-divider"/>
+  <hr className="sidebar-divider"/>
 
   {/* <!-- Heading --> */}
-  <div class="sidebar-heading">Actions</div>
+  <div className="sidebar-heading">Actions</div>
 
   {/* <!-- Nav Item - Pages --> */}
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="/">
-      <i class="fas fa-fw fa-folder"></i>
+  <li className="nav-item">
+    <a className="nav-link collapsed" href="/">
+      <i className="fas fa-fw fa-folder"></i>
       <span>Pages</span>
     </a>
   </li>
 
   {/* <!-- Nav Item - Charts --> */}
-  <li class="nav-item">
-    <a class="nav-link" href="/">
-      <i class="fas fa-fw fa-chart-area"></i>
+  <li className="nav-item">
+    <a className="nav-link" href="/">
+      <i className="fas fa-fw fa-chart-area"></i>
       <span>Charts</span></a>
   </li>
 
   {/* <!-- Nav Item - Tables --> */}
-  <li class="nav-item">
-    <a class="nav-link" href="/">
-      <i class="fas fa-fw fa-table"></i>
+  <li className="nav-item">
+    <a className="nav-link" href="/">
+      <i className="fas fa-fw fa-table"></i>
       <span>Tables</span></a>
   </li>
 
   {/* <!-- Divider --> */}
-  <hr class="sidebar-divider d-none d-md-block"/>
+  <hr className="sidebar-divider d-none d-md-block"/>
 </ul>
 }
 
