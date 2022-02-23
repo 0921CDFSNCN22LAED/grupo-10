a3bad7bfd00da57bad7403f6028c4bc341d8669a
Flechashe <se.amigos@gmail.com>
2022-02-22 21:03:26 -0300

    Centramos el div en la página 404.

--- dashboard/src/components/NotFound.jsx
+++ dashboard/src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function NotFound(){
   return (
-    <div className="text-center d-flex justify-content-center">
+    <div className="text-center d-flex justify-content-center flex-grow-1 align-items-center">
       <div>
         <h1>404: No encontrado.</h1>
         <Link to="/"><h2>Volver al inicio</h2></Link>
