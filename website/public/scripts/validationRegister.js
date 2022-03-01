3bfef5cc2b403de0c9307955ced40f96e7e30362
LuiosD <luisdartayet@gmail.com>
2022-03-01 07:32:15 -0300

    corregí vallidación email existente frontend, agregué favicon, corregí estructura HTML

--- website/public/scripts/validationRegister.js
+++ website/public/scripts/validationRegister.js
@@ -17,51 +17,55 @@ formulario.addEventListener('submit', async (e) => {
     errores.push('Tu nombre debe tener al menos 2 caracteres');
   }
 
-  const emailIsNotUnique = async function(){
-    const response =  await fetch(`http://localhost:3001/api/users/email?email=${email.value}`)
-    const data = await response.json()
-    if(data){
-      return true
+  const emailIsNotUnique = async function () {
+    e.preventDefault();
+    const response = await fetch(
+      `http://localhost:3001/api/users/email?email=${email.value}`
+    );
+    const data = await response.json();
+    if (data) {
+      return true;
     }
-    console.log(data);
-  }
+  };
   if (email.value == '') {
     errores.push('Ingresá tu email');
   } else if (!validator.isEmail(email.value)) {
     errores.push('Tu email debe ser válido');
-  }else if (await emailIsNotUnique()){
-    errores.push("Ya hay una cuenta asociada al email que ingresaste.")
+  } else if (await emailIsNotUnique()) {
+    errores.push('Ya hay una cuenta asociada al email que ingresaste.');
   }
 
-
-
-  const regex = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/
+  const regex =
+    /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/;
   if (password.value == '') {
     errores.push('Ingresá tu contraseña');
   } else if (password.value.length < 8) {
     errores.push('Tu contraseña debe tener al menos 8 caracteres');
-  } else if(!regex.test(password.value)){
-    errores.push('Tu contraseña debe tener una mayuscula, minuscula , un numero y un caracter especial')
+  } else if (!regex.test(password.value)) {
+    errores.push(
+      'Tu contraseña debe tener una mayuscula, minuscula , un numero y un caracter especial'
+    );
   }
 
   if (repassword.value == '') {
     errores.push('Volvé a ingresar tu contraseña');
   } else if (repassword.value != password.value) {
     errores.push('Las contraseñas no coinciden');
-  } 
+  }
 
-  const extension = avatar.value.slice(-4);
-  const allowedExtension = ['jpeg', '.jpg', '.png', '.gif'];
-  if (!allowedExtension.includes(extension)) {
+  const allowedMimeType = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+  const mimeType = avatar.files[0]?.type;
+  if (avatar.value != ' ' && !allowedMimeType.includes(mimeType)) {
     errores.push('Imagen inválida');
   }
 
   if (errores.length > 0) {
     e.preventDefault();
-
     const ulErrores = document.querySelector('div.errores ul');
     for (let i = 0; i < errores.length; i++) {
       ulErrores.innerHTML += '<li>' + errores[i] + '</li>';
     }
+  } else {
+    formulario.submit();
   }
 });
